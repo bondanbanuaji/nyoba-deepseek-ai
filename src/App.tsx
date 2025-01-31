c35b3c277da7d2bd2b1391b6d2c5287519a52a44
jwcfrey <bondanbanuaji@gmail.com>
2025-01-31 14:36:59 +0700

    deepsek inegrasi

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import { MessageCard } from './components/MessageCard';
+import ollama from 'ollama';
 import './App.css'
 
 type Message = {
@@ -21,7 +22,18 @@ function App() {
       setMessages((prevMessages) => [...prevMessages, newMessage]);
 
       // function ai
-      
+      const { message } = await ollama.chat({
+        model: "deepseek-r1:1.5b",
+        messages: [
+          newMessage
+        ],
+        stream: false
+      });
+
+      setMessages((prevMessages) => [...prevMessages, 
+        { role: "assistant", content: message.content },
+      ]);
+
       setInput("");
     }
   };
